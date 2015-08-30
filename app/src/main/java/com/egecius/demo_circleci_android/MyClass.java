8c66c2fd63bd9c3de5337fd2c13776f7b09d0bd7
Egis <e.gecius@gmail.com>
2015-08-30 14:24:31 +0100

    unit 4.12 added 2 passing tests added

---
+++ app/src/main/java/com/egecius/demo_circleci_android/MyClass.java
@@ -0,0 +1,12 @@
+package com.egecius.demo_circleci_android;
+
+/**
+ * Single Responsibility:
+ *
+ * //todo
+ */
+public class MyClass {
+
+	public final String name = "Egis";
+	public final String surname = "Gecius";
+}
---
+++ app/src/test/java/MyClassTest.java
@@ -0,0 +1,33 @@
+import com.egecius.demo_circleci_android.MyClass;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Single Responsibility:
+ *
+ * //todo
+ */
+public class MyClassTest {
+
+	@Test
+	public void firsTest() {
+		MyClass myClass = new MyClass();
+		assertEquals(myClass.name, "Egis");
+	}
+
+	@Test
+	public void passingSurnameTest() {
+		MyClass myClass = new MyClass();
+		assertEquals(myClass.surname, "Gecius");
+	}
+
+//	@Test
+//	public void failingTest() {
+//		MyClass myClass = new MyClass();
+//		assertEquals(myClass.surname, "Dzigajevas");
+//	}
+
+
+}
