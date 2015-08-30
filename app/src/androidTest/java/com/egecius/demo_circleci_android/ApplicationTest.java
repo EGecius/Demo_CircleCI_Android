409dc6c73058b3555aaa61efc1dbb272a50b2a3d
Egis <e.gecius@gmail.com>
2015-08-30 14:49:39 +0100

    lint task added to yml

--- app/src/androidTest/java/com/egecius/demo_circleci_android/ApplicationTest.java
+++ app/src/androidTest/java/com/egecius/demo_circleci_android/ApplicationTest.java
@@ -2,12 +2,19 @@ package com.egecius.demo_circleci_android;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
 	public ApplicationTest() {
 		super(Application.class);
+	}
+
+	@MediumTest
+	public void failingTest() {
+		assertEquals(false, true);
 	}
 }
