f5fd615099e325682d36bb10a97e163c19a34092
Egidijus Gecius <egidijus.gecius@candyspace.com>
2015-09-15 09:25:21 +0100

    build fixed

--- playerimpl/src/main/java/com/example/egidijusgecius/playerimpl/MainActivity.java
+++ playerimpl/src/main/java/com/example/egidijusgecius/playerimpl/MainActivity.java
@@ -1,16 +1,14 @@
-package com.egecius.androidlibrary;
+package com.example.egidijusgecius.playerimpl;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /** library Activity */
-public class LibraryActivity extends Activity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_library);
+		setContentView(R.layout.activity_main);
 	}
 }
